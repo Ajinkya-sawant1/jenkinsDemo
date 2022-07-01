658564b796d5186fb334b4664468122f4c1437b2
Ajinkya Sawant <sawant.ajinkya12345@gmail.com>
2022-07-01 23:41:32 +0530

    scanner removed

--- Calculator.java
+++ Calculator.java
@@ -6,52 +6,27 @@ class Calculator {
     char operator;
     Double number1, number2, result;
 
-    Scanner input = new Scanner(System.in);
 
-  
-    System.out.println("Choose an operator: +, -, *, or /");
-    operator = input.next().charAt(0);
+  	 number1 = 9.0;
 
-  
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
-
-    System.out.println("Enter second number");
-    number2 = input.nextDouble();
-
-	System.out.println("Added new Changes");
-
-    switch (operator) {
+ 	 number2 = 5.0;
 
       
-      case '+':
+
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
-        break;
 
      
-      case '-':
+
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
-        break;
+    
 
-     
-      case '*':
+
         result = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + result);
-        break;
-
+   
      
-      case '/':
-        result = number1 / number2;
-        System.out.println(number1 + " / " + number2 + " = " + result);
-        break;
-
-      default:
-        System.out.println("Invalid operator!");
-        break;
-    }
-
-    input.close();
+     
   }
 }
